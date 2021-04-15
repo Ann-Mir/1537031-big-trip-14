e26341fcf70b3f3f04679a66523953f6fc07a46a
Anna Mironovich <anna.mail787@gmail.com>
2021-04-15 23:46:21 +0300

    formatted mark-up in views

--- src/view/event.js
+++ src/view/event.js
@@ -5,10 +5,10 @@ import {humanizeDate, humanizeDuration, humanizeFullDate, humanizeTime} from '..
 const createOffersTemplate = (offers) => {
   const offersList = offers.map((offer) => {
     return `<li class="event__offer">
-        <span class="event__offer-title">${offer.title}</span>
-        &plus;&euro;&nbsp;
-        <span class="event__offer-price">${offer.price}</span>
-      </li>`;
+              <span class="event__offer-title">${offer.title}</span>
+              &plus;&euro;&nbsp;
+              <span class="event__offer-price">${offer.price}</span>
+            </li>`;
   }).join('');
   return offersList;
 };
--- src/view/trip-info.js
+++ src/view/trip-info.js
@@ -22,16 +22,16 @@ const createTripInfoTemplate = (points) => {
   });
 
   return `<section class="trip-main__trip-info  trip-info">
-      <div class="trip-info__main">
-        <h1 class="trip-info__title">${route}</h1>
+            <div class="trip-info__main">
+              <h1 class="trip-info__title">${route}</h1>
 
-        <p class="trip-info__dates">${getEventPeriod(points[0], points[points.length - 1])}</p>
-      </div>
+              <p class="trip-info__dates">${getEventPeriod(points[0], points[points.length - 1])}</p>
+            </div>
 
-      <p class="trip-info__cost">
-        Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
-      </p>
-    </section>`;
+            <p class="trip-info__cost">
+              Total: &euro;&nbsp;<span class="trip-info__cost-value">${totalPrice}</span>
+            </p>
+          </section>`;
 };
 
 export default class TripInfo extends AbstractView {
