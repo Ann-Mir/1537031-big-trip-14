e8739883daf010e099d9863e7a91fe1a21d74ca0
Anna Mironovich <anna.mail787@gmail.com>
2021-04-16 14:04:57 +0300

    added sort view

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,8 +1,9 @@
 import AbstractView from './abstract.js';
+import {SortType} from '../utils/constants';
 
 const createTripEventsSortForm = () => {
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-            <div class="trip-sort__item  trip-sort__item--day">
+            <div class="trip-sort__item  trip-sort__item--day" data-sort-type="${SortType.DAY}">
               <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day">
               <label class="trip-sort__btn" for="sort-day">Day</label>
             </div>
@@ -12,12 +13,12 @@ const createTripEventsSortForm = () => {
               <label class="trip-sort__btn" for="sort-event">Event</label>
             </div>
 
-            <div class="trip-sort__item  trip-sort__item--time">
+            <div class="trip-sort__item  trip-sort__item--time" data-sort-type="${SortType.TIME}">
               <input id="sort-time" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-time">
               <label class="trip-sort__btn" for="sort-time">Time</label>
             </div>
 
-            <div class="trip-sort__item  trip-sort__item--price">
+            <div class="trip-sort__item  trip-sort__item--price" data-sort-type="${SortType.PRICE}">
               <input id="sort-price" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-price" checked>
               <label class="trip-sort__btn" for="sort-price">Price</label>
             </div>
@@ -31,8 +32,28 @@ const createTripEventsSortForm = () => {
 
 export default class SortView extends AbstractView {
 
+  constructor() {
+    super();
+    this._sortTypeChangeHandler = this._sortTypeChangeHandler.bind(this);
+  }
+
   getTemplate() {
     return createTripEventsSortForm();
+  }
+
+  _sortTypeChangeHandler(evt) {
+    if (!evt.target.classList.contains('trip-sort__item')) {
+      return;
+    }
+
+    evt.preventDefault();
+    this._callback.sortTypeChange(evt.target.dataset.sortType);
+
+  }
+
+  setSortTypeChangeHandler(callback) {
+    this._callback.sortTypeChange = callback;
+    this.getElement().addEventListener('click', this._sortTypeChangeHandler);
   }
 
 }
