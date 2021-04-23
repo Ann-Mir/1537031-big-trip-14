c983298cfd85d64cfb6224f0d7dfeec2e987e1de
Anna Mironovich <anna.mail787@gmail.com>
2021-04-23 11:51:43 +0300

    hides offers section for null offers

--- src/view/trip-event-edit.js
+++ src/view/trip-event-edit.js
@@ -36,7 +36,7 @@ const createDestinationsOptionsTemplate = () => {
 };
 
 export const createEditPointTemplate = (point=DEFAULT_POINT) => {
-
+  const hasOffers = point.offers.length > 0;
   const type = capitalizeFirstLetter(point.type);
   return `<li class="trip-events__item">
             <form class="event event--edit" action="#" method="post">
@@ -89,7 +89,7 @@ export const createEditPointTemplate = (point=DEFAULT_POINT) => {
                 </button>
               </header>
               <section class="event__details">
-                <section class="event__section  event__section--offers">
+                <section class="event__section  event__section--offers ${hasOffers ? '' : 'visually-hidden'}">
                   <h3 class="event__section-title  event__section-title--offers">Offers</h3>
                   <div class="event__available-offers">
                     ${createAvailableOffersTemplate(point.type, point.offers)}
