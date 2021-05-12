da5cd0a17dca304ea1662d51c366af5b23e61e7b
Anna Mironovich <anna.mail787@gmail.com>
2021-05-12 18:23:12 +0300

    add destination validation

--- src/view/trip-event-edit.js
+++ src/view/trip-event-edit.js
@@ -88,7 +88,7 @@ const createEditPointTemplate = (availableOffers, destinations, state, mode= Mod
                   <label class="event__label  event__type-output" for="event-destination-1">
                     ${typeName}
                   </label>
-                  <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${hasDestination ? he.encode(destination.name) : ''}" list="destination-list-1">
+                  <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${hasDestination ? he.encode(destination.name) : ''}" list="destination-list-1" required>
                   <datalist id="destination-list-1">
                     ${createDestinationsOptionsTemplate(destinations)}
                   </datalist>
