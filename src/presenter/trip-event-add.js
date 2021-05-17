3949324ad069d5c84146dfea13eb3897734adb85
Anna Mironovich <anna.mail787@gmail.com>
2021-05-17 23:37:59 +0300

    enables new event button upon esc

--- src/presenter/trip-event-add.js
+++ src/presenter/trip-event-add.js
@@ -51,6 +51,7 @@ export default class TripEventAdd {
   _escKeyDownHandler(evt) {
     if (evt.key === 'Escape' || evt.key === 'Esc') {
       evt.preventDefault();
+      this._newEventButtonComponent.enable();
       this.destroy();
     }
   }
