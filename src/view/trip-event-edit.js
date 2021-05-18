4bebf9afd22794003429aa11d542198a243717fe
Anna Mironovich <anna.mail787@gmail.com>
2021-05-18 07:17:54 +0300

    refactores update state

--- src/view/trip-event-edit.js
+++ src/view/trip-event-edit.js
@@ -330,7 +330,7 @@ export default class TripEventEdit extends SmartView {
       {
         offers,
         type,
-        hasOffers: availableOffers && availableOffers.length > 0 ? true : false,
+        hasOffers: !!(availableOffers && availableOffers.length > 0),
       },
     );
   }
